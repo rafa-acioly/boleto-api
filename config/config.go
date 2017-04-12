e97ed0fb9f78a3d3af0872239c4b19b80ee1a75d
Philippe Moneda <philippe.moneda@mundipagg.com>
2017-04-12 14:25:38 -0300

    :construction: Adiciona tag de versão na configuração

--- config/config.go
+++ config/config.go
@@ -3,12 +3,14 @@ package config
 //Config é a estrutura que tem todas as configurações da aplicação
 type Config struct {
 	APIPort string
+	Version string
 }
 
 //GetConfig retorna o objeto de configurações da aplicação
 func GetConfig() Config {
 	cnf := Config{
 		APIPort: ":3000",
+		Version: "0.0.1",
 	}
 	return cnf
 }
