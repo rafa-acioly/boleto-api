5e9b178e1f72819987c17dd1b4368350a15e3e00
Munir Wanis <fallen.angel.pp@gmail.com>
2017-04-12 19:17:41 -0300

    :white_check_mark: Cria testes para entidades

---
+++ models/model_test.go
@@ -0,0 +1,92 @@
+package models
+
+import "testing"
+import "time"
+
+func TestShouldReturnValidCpfOnDocumentType(t *testing.T) {
+	document := Document{Number: "12345678901", Type: "CPF"}
+	if document.Type.IsCpf() == false {
+		t.Fail()
+	}
+}
+
+func TestShouldReturnInvalidCpfOnDocumentType(t *testing.T) {
+	document := Document{Number: "1234567890132", Type: "CNPJ"}
+	if document.Type.IsCpf() {
+		t.Fail()
+	}
+}
+
+func TestShouldReturnValidCnpjOnDocumentType(t *testing.T) {
+	document := Document{Number: "1234567890132", Type: "CnpJ"}
+	if document.Type.IsCnpj() == false {
+		t.Fail()
+	}
+}
+
+func TestShouldReturnInvalidCnpjOnDocumentType(t *testing.T) {
+	document := Document{Number: "12345678901", Type: "CPF"}
+	if document.Type.IsCnpj() {
+		t.Fail()
+	}
+}
+
+func TestShouldReturnValidCpfOnDocumentNumber(t *testing.T) {
+	document := Document{Number: "12345678901", Type: "CPF"}
+	if document.Number.IsCpf() == false {
+		t.Fail()
+	}
+}
+
+func TestShouldReturnInvalidCpfOnDocumentNumber(t *testing.T) {
+	document := Document{Number: "12345678901asd", Type: "CPF"}
+	if document.Number.IsCpf() {
+		t.Fail()
+	}
+}
+
+func TestShouldReturnValidCnpjOnDocumentNumber(t *testing.T) {
+	document := Document{Number: "12345678901564", Type: "CNPJ"}
+	if document.Number.IsCnpj() == false {
+		t.Fail()
+	}
+}
+
+func TestShouldReturnInvalidCnpjOnDocumentNumber(t *testing.T) {
+	document := Document{Number: "12345678901564asdf", Type: "CNPJ"}
+	if document.Number.IsCnpj() {
+		t.Fail()
+	}
+}
+
+func TestShouldReturnNewTitle(t *testing.T) {
+	expDate := time.Now().AddDate(0, 0, 6).Format("2006-01-02")
+	_, err := NewTitle(expDate, 100, 231654)
+	if err != nil {
+		t.Fail()
+	}
+}
+
+func TestShouldCreateNewTitleWithEqualExpireDateAndCreateDate(t *testing.T) {
+	expDate := time.Now().Format("2006-01-02")
+	_, err := NewTitle(expDate, 100, 231654)
+	if err != nil {
+		t.Fail()
+	}
+}
+
+func TestShouldFailWithCreateDateBiggerThanExpireDate(t *testing.T) {
+	expDate := time.Now().AddDate(0, 0, -6).Format("2006-01-02")
+	_, err := NewTitle(expDate, 100, 231654)
+	if err == nil {
+		t.Fail()
+	}
+}
+
+func TestShouldFailWithAmountInCentsMinorThanOne(t *testing.T) {
+	expDate := time.Now().AddDate(0, 0, 6).Format("2006-01-02")
+	_, err := NewTitle(expDate, 0, 231654)
+	if err == nil {
+		t.Fail()
+	}
+}
