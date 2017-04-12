c4372f3c5e95e05e72ef45d161441333d4a60cbf
Munir Wanis <fallen.angel.pp@gmail.com>
2017-04-12 19:49:59 -0300

    :construction: Cria constante com os números dos banco

--- models/boleto.go
+++ models/boleto.go
@@ -6,8 +6,23 @@ type BoletoRequest struct {
 	Agreement      Agreement
 	Title          Title
 	Buyer          Buyer
+	BankNumber     int
 }
 
 // BoletoResponse entidade de saída para o boleto
 type BoletoResponse struct {
 }
+
+const (
+	// BancoDoBrasil constante do Banco do Brasil
+	BancoDoBrasil = 1
+
+	// Santander constante do Santander
+	Santander = 33
+
+	// Itau constante do Itau
+	Itau = 341
+
+	// Bradesco constante do Bradesco
+	Bradesco = 237
+)
