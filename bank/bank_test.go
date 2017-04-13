08424ebcf83c56dbb9d64da0e7b97e3373a44477
Philippe Moneda <philippe.moneda@mundipagg.com>
2017-04-13 11:12:44 -0300

    :construction: adiciona a estrategia de proxy de bancos para integração e a parte do Banco do Brasil

---
+++ bank/bank_test.go
@@ -0,0 +1,18 @@
+package bank
+
+import (
+	"testing"
+
+	"bitbucket.org/mundipagg/boletoapi/models"
+	"bitbucket.org/mundipagg/boletoapi/test"
+)
+
+func TestShouldExecuteBBStrategy(t *testing.T) {
+	InstallBanks()
+	var bb models.BankNumber = models.BancoDoBrasil
+	bank, err := Get(bb)
+	number := bank.GetBankNumber()
+	test.ExpectNoError(err, t)
+	test.ExpectTrue(number.IsBankNumberValid(), t)
+	test.ExpectTrue(number == models.BancoDoBrasil, t)
+}
--- bank/bb.go
+++ bank/bb.go
@@ -12,5 +12,10 @@ func (b bankBB) Login() auth.Token {
 	return auth.Token{}
 }
 func (b bankBB) RegisterBoleto(boleto models.BoletoRequest) models.BoletoResponse {
+	//fmt.Println("Registrando boleto no Banco do Brasil")
+
 	return models.BoletoResponse{}
+}
+func (b bankBB) GetBankNumber() models.BankNumber {
+	return models.BancoDoBrasil
 }
