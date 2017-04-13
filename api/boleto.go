3cc56964dfabaa1c1176f0ebca522c6aaa6f3d23
Philippe Moneda <philippe.moneda@mundipagg.com>
2017-04-13 11:13:20 -0300

    :construction: faz o bind do boleto request no registerBoleto

--- api/boleto.go
+++ api/boleto.go
@@ -1,8 +1,22 @@
 package api
 
-import gin "gopkg.in/gin-gonic/gin.v1"
+import (
+	"net/http"
+
+	"bitbucket.org/mundipagg/boletoapi/bank"
+	"bitbucket.org/mundipagg/boletoapi/models"
+	gin "gopkg.in/gin-gonic/gin.v1"
+)
 
 //Regista um boleto em um determinado banco
 func registerBoleto(c *gin.Context) {
-	c.JSON(200, gin.H{"ok": "OK"})
+	boleto := models.BoletoRequest{}
+	c.BindJSON(&boleto)
+	bank, err := bank.Get(boleto.BankNumber)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, errorResponse{Code: "001", Message: err.Error()})
+		return
+	}
+	bank.RegisterBoleto(boleto)
+	c.JSON(200, boleto)
 }
