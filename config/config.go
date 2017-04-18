e72e4ec70eaa796c242580c0212bbcd55ab5370d
Munir Wanis <mwanis@mundipagg.com>
2017-04-18 17:25:20 -0300

    :wrench: Cria variável Environment e muda SEQ para localhost

--- config/config.go
+++ config/config.go
@@ -8,6 +8,7 @@ type Config struct {
 	SEQAPIKey          string
 	EnableRequestLog   bool
 	EnablePrintRequest bool
+	Environment        string
 }
 
 //Get retorna o objeto de configurações da aplicação
@@ -15,10 +16,10 @@ func Get() Config {
 	cnf := Config{
 		APIPort:            ":3000",
 		Version:            "0.0.1",
-		SEQUrl:             "http://stglog.mundipagg.com/", //Pegar o SEQ de dev
-		SEQAPIKey:          "4jZzTybZ9bUHtJiPdh6",
-		EnableRequestLog:   false, // Log a cada request no SEQ
-		EnablePrintRequest: true,  // Imprime algumas informacoes da request no console
+		SEQUrl:             "http://localhost:5341/", //Pegar o SEQ de dev // SEQAPIKey:          "4jZzTybZ9bUHtJiPdh6",
+		EnableRequestLog:   false,                    // Log a cada request no SEQ
+		EnablePrintRequest: true,                     // Imprime algumas informacoes da request no console
+		Environment:        "Development",
 	}
 	return cnf
 }
