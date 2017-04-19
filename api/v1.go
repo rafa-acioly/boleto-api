27dc0bf72d1d9d5cf92e755e9b984cc0e18982c1
Philippe Moneda <philippe.moneda@mundipagg.com>
2017-04-19 17:52:16 -0300

    :construction: adiciona servico para mostrar as configurações da aplicação em execução

--- api/v1.go
+++ api/v1.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"bitbucket.org/mundipagg/boletoapi/config"
 	gin "gopkg.in/gin-gonic/gin.v1"
 )
 
@@ -9,4 +10,7 @@ func InstallV1(router *gin.Engine) {
 	v1 := router.Group("v1")
 	v1.POST("/boleto/register", registerBoleto)
 	v1.GET("/boleto", getBoleto)
+	v1.GET("/info", func(c *gin.Context) {
+		c.JSON(200, config.Get())
+	})
 }
