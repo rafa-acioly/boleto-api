6c6987b18f9b6063fa61a00903c7b19b6cbc3a31
Munir Wanis <fallen.angel.pp@gmail.com>
2017-04-19 16:43:22 -0300

    :speaker: Melhora log com a nova versão do Goseq

--- log/seq.go
+++ log/seq.go
@@ -3,10 +3,7 @@ package log
 import (
 	"net/http"
 
-	"fmt"
-
 	"bitbucket.org/mundipagg/boletoapi/config"
-	"bitbucket.org/mundipagg/boletoapi/models"
 	"github.com/mundipagg/goseq"
 )
 
@@ -16,13 +13,16 @@ var messageType string
 // Operation a operacao usada na API
 var Operation string
 
+// Recipient o nome do banco
+var Recipient string
+
 //Install instala o "servico" de log do SEQ
 func Install() error {
 	_logger, err := goseq.GetLogger(config.Get().SEQUrl, config.Get().SEQAPIKey)
 	if err != nil {
 		return err
 	}
-	_logger.SetDefaultProperties(map[string]string{
+	_logger.SetDefaultProperties(map[string]interface{}{
 		"Application": config.Get().ApplicationName,
 		"Environment": config.Get().Environment,
 		"Domain":      config.Get().SEQDomain,
@@ -36,61 +36,55 @@ func formatter(message string) string {
 }
 
 // Request loga o request para algum banco
-func Request(content, url string, headers http.Header, bankNumber models.BankNumber) {
-	messageType = "Request"
-	props := goseq.NewProperties()
-	props.AddProperty("MessageType", messageType)
-	props.AddProperty("Content", content)
-	props.AddProperty("BankName", bankNumber.BankName())
-	props.AddProperty("Headers", formatHeaders(headers))
-	props.AddProperty("Operation", Operation)
-	props.AddProperty("URL", url)
+func Request(content interface{}, url string, headers http.Header) {
+	go (func() {
+		messageType = "Request"
+		props := goseq.NewProperties()
+		props.AddProperty("MessageType", messageType)
+		props.AddProperty("Content", content)
+		props.AddProperty("Recipient", Recipient)
+		props.AddProperty("Headers", headers)
+		props.AddProperty("Operation", Operation)
+		props.AddProperty("URL", url)
 
-	msg := formatter("to {BankName} ({URL})")
+		msg := formatter("to {Recipient} ({URL})")
 
-	logger.Information(msg, props)
+		logger.Information(msg, props)
+	})()
 }
 
 // Response loga o response para algum banco
-func Response(content string, bankNumber models.BankNumber) {
-	messageType = "Response"
-	props := goseq.NewProperties()
-	props.AddProperty("MessageType", messageType)
-	props.AddProperty("Content", content)
-	props.AddProperty("BankName", bankNumber.BankName())
-	props.AddProperty("Operation", Operation)
+func Response(content interface{}) {
+	go (func() {
+		messageType = "Response"
+		props := goseq.NewProperties()
+		props.AddProperty("MessageType", messageType)
+		props.AddProperty("Content", content)
+		props.AddProperty("Recipient", Recipient)
+		props.AddProperty("Operation", Operation)
 
-	msg := formatter("from {BankName}")
+		msg := formatter("from {Recipient}")
 
-	logger.Information(msg, props)
+		logger.Information(msg, props)
+	})()
 }
 
 //Info loga mensagem do level INFO
 func Info(msg string) {
-	logger.Information(msg, goseq.NewProperties())
+	go logger.Information(msg, goseq.NewProperties())
 }
 
 //Warn loga mensagem do leve Warning
 func Warn(msg string) {
-	logger.Warning(msg, goseq.NewProperties())
+	go logger.Warning(msg, goseq.NewProperties())
 }
 
 //Fatal loga mensagem do leve fatal
 func Fatal(msg string) {
-	logger.Fatal(msg, goseq.NewProperties())
+	go logger.Fatal(msg, goseq.NewProperties())
 }
 
 //Close fecha a conexao com o SEQ
 func Close() {
 	logger.Close()
-}
-
-func formatHeaders(headers http.Header) string {
-	s := ""
-	for name, headers := range headers {
-		for _, h := range headers {
-			s += fmt.Sprintf("%v: %v\n", name, h)
-		}
-	}
-	return s
 }
