6556ef062b4f674faab17b04f2ace2484cb90494
Munir Wanis <fallen.angel.pp@gmail.com>
2017-04-19 16:42:55 -0300

    :fire: Remove código não usado do middleware

--- api/middlewares.go
+++ api/middlewares.go
@@ -1,25 +1,12 @@
 package api
 
 import (
-	"fmt"
-
-	"bitbucket.org/mundipagg/boletoapi/log"
 	gin "gopkg.in/gin-gonic/gin.v1"
 )
-
-//RequestLogger faz o log no SEQ para toda request
-func RequestLogger() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		fmt.Println("Ola")
-		log.Info("Teste SEQ")
-		c.Next()
-	}
-}
 
 // ReturnHeaders 'seta' os headers padrões de resposta
 func ReturnHeaders() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		log.Info("Registrando Headers")
 		c.Header("Content-Type", "application/json")
 		c.Next()
 	}
