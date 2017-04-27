96adbff003d1b8797bf52b22e191565e270eed64
Munir Wanis <fallen.angel.pp@gmail.com>
2017-04-26 23:48:35 -0300

    :white_check_mark: Adiciona teste para cálculo de dígito da conta

--- bank/bb_test.go
+++ bank/bb_test.go
@@ -15,3 +15,13 @@ func TestShouldCalculateAgencyDigitFromBb(t *testing.T) {
 
 	test.ExpectTrue(agencyDigitCalculator("5797") == "5", t)
 }
+
+func TestShouldCalculateAccountDigitFromBb(t *testing.T) {
+	test.ExpectTrue(accountDigitCalculator("", "00006685") == "0", t)
+
+	test.ExpectTrue(accountDigitCalculator("", "00025619") == "6", t)
+
+	test.ExpectTrue(accountDigitCalculator("", "00006842") == "X", t)
+
+	test.ExpectTrue(accountDigitCalculator("", "00000787") == "0", t)
+}
