3fe0a902111f3f5a4ee5b4abfe13c911a3faeeb8
Munir Wanis <fallen.angel.pp@gmail.com>
2017-04-28 15:13:01 -0300

    :poop:

--- build/build.sh
+++ build/build.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
 
-if [[ "$1" != "" ]]; then
-    PROJECTPATH="$1"
-    echo "Changing directory to path $1"
-    cd $PROJECTPATH
-
-    echo "Installing dependencies with glide"
-    pwd
-    glide install
-
-    echo "Starting build"
-    go build -v
-else
-    echo "[ERROR] Expecting build directory as argument"
-    exit 1
-fi
+PROJECTPATH=$GOPATH/src/bitbucket.org/mundipagg/boletoapi
+echo "Changing directory to path $PROJECTPATH"
+cd $PROJECTPATH
+echo "Installing dependencies with glide"
+glide install
+echo "Starting build"
+go build -v
--- build/test.sh
+++ build/test.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-if [[ "$1" != "" ]]; then
-    PROJECTPATH="$1"
-    echo "Changing directory to path $1"
-    cd $PROJECTPATH
-
-    echo "Starting tests"
-    go test $(go list ./... | grep -v /vendor/) -v
-else
-    echo "[ERROR] Expecting build directory as argument"
-    exit 1
-fi
+PROJECTPATH=$GOPATH/src/bitbucket.org/mundipagg/boletoapi
+echo "Changing directory to path $PROJECTPATH"
+cd $PROJECTPATH
+echo "Starting tests"
+go test $(go list ./... | grep -v /vendor/) -v
