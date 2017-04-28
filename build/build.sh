1a77a8fb93120fe94bb62ac720ba35dffa3258f1
Munir Wanis <fallen.angel.pp@gmail.com>
2017-04-28 15:50:33 -0300

    :poop:

--- build/build.sh
+++ build/build.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-GG="/home/mundipagg/go"
-PROJECTPATH="$GG/src/bitbucket.org/mundipagg/boletoapi"
+export GOPATH="/home/mundipagg/go"
+PROJECTPATH="$GOPATH/src/bitbucket.org/mundipagg/boletoapi"
 echo "Changing directory to path $PROJECTPATH"
 ls -la $PROJECTPATH
 cd $PROJECTPATH
 echo "Installing dependencies with glide"
+
 go get
 go run main.go
 echo "Starting build"
