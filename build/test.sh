0d956bc956fba17df8ca991cd41bd709998291ca
Munir Wanis <fallen.angel.pp@gmail.com>
2017-04-28 00:57:10 -0300

    :construction: Muda test.sh para testar no tfs

--- build/test.sh
+++ build/test.sh
@@ -1,4 +1,11 @@
-echo "Rodando os testes"
-PROJECTPATH=$GOPATH/src/bitbucket.org/mundipagg/boletoapi
-cd $PROJECTPATH
+#!/bin/bash
+echo "Starting tests"
+
+glide install
+
 go test $(go list ./... | grep -v /vendor/) -v
+
+# echo "Rodando os testes"
+# PROJECTPATH=$GOPATH/src/bitbucket.org/mundipagg/boletoapi
+# cd $PROJECTPATH
+# go test $(go list ./... | grep -v /vendor/) -v
