ca289bba16dcc0d036f7fd6681b999b5b275b6fa
Munir Wanis <fallen.angel.pp@gmail.com>
2017-04-28 01:58:22 -0300

    :art: Melhora script de build

--- build/build.sh
+++ build/build.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-# PROJECTPATH=$GOPATH/src/bitbucket.org/mundipagg/boletoapi
+if [[ "$1" != "" ]]; then
+    PROJECTPATH="$1"
+    echo "Changing directory to path $1"
+    cd $PROJECTPATH
 
-# echo "Mudando para o diretório no repositório no GOPATH"
-# cd $PROJECTPATH
+    echo "Installing dependencies with glide"
+    glide install
 
-pwd
+    echo "Starting build"
+    go build -v
 
-echo "Instalando dependências com o glide"
-glide install
-
-echo "Fazendo o build do projeto"
-go build -v
+    exit 0
+else
+    echo "[ERROR] Expecting build directory as argument"
+    exit 1
+fi
--- build/test.sh
+++ build/test.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
-echo "Starting tests"
 
-# PROJECTPATH=$GOPATH/src/bitbucket.org/mundipagg/boletoapi
+if [[ "$1" != "" ]]; then
+    PROJECTPATH="$1"
+    echo "Changing directory to path $1"
+    cd $PROJECTPATH
 
-# cd $PROJECTPATH
+    echo "Starting tests"
+    go test $(go list ./... | grep -v /vendor/) -v
 
-pwd
-
-go test $(go list ./... | grep -v /vendor/) -v
+    exit 0
+else
+    echo "[ERROR] Expecting build directory as argument"
+    exit 1
+fi
