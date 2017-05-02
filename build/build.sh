58094365e87a4d86fa0d947f25654f4cf30eecaa
Pedro Santiago <psantiago@mundipagg.com>
2017-05-02 15:04:30 -0300

    :construction: Adiciona comandos para debug no script de build

--- build/build.sh
+++ build/build.sh
@@ -5,9 +5,19 @@ PROJECTPATH="$GOPATH/src/bitbucket.org/mundipagg/boletoapi"
 echo "Changing directory to path $PROJECTPATH"
 ls -la $PROJECTPATH
 cd $PROJECTPATH
-echo "Installing dependencies with glide"
 
+echo "ENV:"
+env
+
+echo "GO ENV:"
+go env
+
+echo "GO PATH:"
+echo "$GOPATH"
+
+echo "Installing dependencies with glide"
 go get
 go run main.go
+
 echo "Starting build"
 go build -v
