2c61a37ac9fba7444c6016c7189c1a1ba3f10c87
Pedro Santiago <psantiago@mundipagg.com>
2017-05-02 15:26:54 -0300

    :construction: Adiciona comando para exportar GOPATH no script de build

--- build/build.sh
+++ build/build.sh
@@ -6,14 +6,19 @@ echo "Changing directory to path $PROJECTPATH"
 ls -la $PROJECTPATH
 cd $PROJECTPATH
 
+echo "GO PATH:"
+echo "$GOPATH"
+
+export GOPATH="/home/mundipagg/go"
+
+echo "GO PATH MODIFICADO:"
+echo "$GOPATH"
+
 echo "ENV:"
 env
 
 echo "GO ENV:"
 go env
-
-echo "GO PATH:"
-echo "$GOPATH"
 
 echo "Installing dependencies with glide"
 go get
