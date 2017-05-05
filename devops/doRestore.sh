13e48d39e6cedce8e0920a9091f760cd769ccf79
Philippe Moneda <philippe.moneda@mundipagg.com>
2017-05-05 18:01:10 -0300

    :construction: script de restore do mongo criado: usando a ferramenta mongorestore

--- devops/doRestore.sh
+++ devops/doRestore.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 CURRENT=`pwd`
-mv bkp_mongo.tar ~/
-docker-compose down
-cd $HOME
-rm -rf $HOME/boletodb
-tar -zxf bkp_mongo.tar
-mv $HOME/bkp_mongo.tar $CURRENT
+BASE_DIR=$HOME/backups
+#limpa a pasta de dump atual
+sudo rm -rf $HOME/dump_boletodb/*
+echo "Informe a data do arquivo de backup que será carregado:YYYY-MM-DD"
+read BCK_DATE
+cd $BASE_DIR
+tar -xvf bck_boletoapi-$BCK_DATE.tar
+ls -la $HOME/dump_boletodb
+cp -r dump_boletodb/* $HOME/dump_boletodb/
+echo "----------------------------------------"
+ls -la $HOME/dump_boletodb
+sudo docker exec -i -t mongodb mongorestore
+rm -rf dump_boletodb
 cd $CURRENT
-docker-compose up -d
