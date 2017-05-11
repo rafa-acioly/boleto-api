459153dafe6bcde2f72ac98600776656e95ad59b
Philippe Moneda <philippe.moneda@mundipagg.com>
2017-05-11 17:55:11 -0300

    :art: atualiza mensagem do body no teste

--- integrationTests/boleto_test.go
+++ integrationTests/boleto_test.go
@@ -1,6 +1,7 @@
 package integrationTests
 
 import (
+	"strings"
 	"testing"
 
 	. "github.com/smartystreets/goconvey/convey"
@@ -16,61 +17,108 @@ import (
 
 const body = `
 {
-    "Agreement": {
-        "Account": "1231231",
-        "AccountDigit": "3",
-        "Agency": "1233",
-        "AgencyDigit": "2",
-        "AgreementNumber": 1014051,
-        "Wallet": 17,
-        "WalletVariation": 19
-    },
-    "Authentication": {
-        "Password": "eyJpZCI6ImY1NzViYjgtYjBiNy00YSIsImNvZGlnb1B1YmxpY2Fkb3IiOjEwOSwiY29kaWdvU29mdHdhcmUiOjEsInNlcXVlbmNpYWxJbnN0YWxhY2FvIjoxLCJzZXF1ZW5jaWFsQ3JlZGVuY2lhbCI6MX0",
-        "Username": "eyJpZCI6IjgwNDNiNTMtZjQ5Mi00YyIsImNvZGlnb1B1YmxpY2Fkb3IiOjEwOSwiY29kaWdvU29mdHdhcmUiOjEsInNlcXVlbmNpYWxJbnN0YWxhY2FvIjoxfQ"
-    },
+
     "BankNumber": 1,
-    "Buyer": {
-        "Address": {
-            "City": "rio de janeiro",
-            "Complement": "bloco 3 apto1306",
-            "District": "tijuca",
-            "Number": "51",
-            "StateCode": "rj",
-            "Street": "rua morais e silva",
-            "ZipCode": "12345678"
-        },
-        "Document": {
-            "Number": "73400584000166",
-            "Type": "CNPJ"
-        },
-        "Name": "moneda"
+
+    "Authentication": {
+
+        "Username": "eyJpZCI6IjgwNDNiNTMtZjQ5Mi00YyIsImNvZGlnb1B1YmxpY2Fkb3IiOjEwOSwiY29kaWdvU29mdHdhcmUiOjEsInNlcXVlbmNpYWxJbnN0YWxhY2FvIjoxfQ",
+
+        "Password": "eyJpZCI6IjBjZDFlMGQtN2UyNC00MGQyLWI0YSIsImNvZGlnb1B1YmxpY2Fkb3IiOjEwOSwiY29kaWdvU29mdHdhcmUiOjEsInNlcXVlbmNpYWxJbnN0YWxhY2FvIjoxLCJzZXF1ZW5jaWFsQ3JlZGVuY2lhbCI6MX0"
+
     },
-    "Recipient": {
-        "Address": {
-            "City": "rio de janeiro",
-            "Complement": "2º Piso loja 404",
-            "District": "tijuca",
-            "Number": "321",
-            "StateCode": "rj",
-            "Street": "rua da loja",
-            "ZipCode": "112312342"
-        },
-        "Document": {
-            "Number": "12312312312366",
-            "Type": "CNPJ"
-        },
-        "Name": "moneda"
+
+    "Agreement": {
+
+        "AgreementNumber": 1014051,
+
+        "WalletVariation": 19,
+
+        "Agency":"5797",
+
+        "Account":"6685"
+
     },
+
     "Title": {
-        "AmountInCents": 30000,
-        "CreateDate": "0001-01-01T00:00:00Z",
+
         "ExpireDate": "2017-05-20",
-        "ExpireDateTime": "0001-01-01T00:00:00Z",
-        "Instructions": "",
-        "OurNumber": 101405218
+
+        "AmountInCents": 200,
+
+        "OurNumber": 101405190,
+
+        "Instructions": "Senhor caixa, após o vencimento não aceitar o pagamento",
+
+        "DocumentNumber": "123456"
+
+    },
+
+    "Buyer": {
+
+        "Name": "Mundipagg Tecnologia em Pagamentos",
+
+        "Document": {
+
+            "Type": "CNPJ",
+
+            "Number": "73400584000166"
+
+        },
+
+        "Address": {
+
+            "Street": "R. Conde de Bonfim",
+
+            "Number": "123",
+
+            "Complement": "Apto",
+
+            "ZipCode": "20520051",
+
+            "City": "Rio de Janeiro",
+
+            "District": "Tijuca",
+
+            "StateCode": "RJ"
+
+        }
+
+    },
+
+    "Recipient": {
+
+      "Name": "Mundipagg Tecnologia em Pagamentos",
+
+        "Document": {
+
+            "Type": "CNPJ",
+
+            "Number": "73400584000166"
+
+        },
+
+        "Address": {
+
+            "Street": "R. Conde de Bonfim",
+
+            "Number": "123",
+
+            "Complement": "Apto",
+
+            "ZipCode": "20520051",
+
+            "City": "Rio de Janeiro",
+
+            "District": "Tijuca",
+
+            "StateCode": "RJ"
+
+        }
+
     }
-}	
+
+}
 `
 
 func getBody() string {
@@ -82,7 +130,7 @@ func getBody() string {
 }
 
 func TestRegisterBoletoRequest(t *testing.T) {
-	go app.Run(true, true, true)
+	go app.Run(true, true, false)
 
 	Convey("deve-se registrar um boleto e retornar as informações de url, linha digitável e código de barras", t, func() {
 		response, st, err := util.Post("http://localhost:3000/v1/boleto/register", getBody(), nil)
@@ -92,11 +140,11 @@ func TestRegisterBoletoRequest(t *testing.T) {
 		errJSON := json.Unmarshal([]byte(response), &boleto)
 		So(errJSON, ShouldEqual, nil)
 		Convey("Se o boleto foi registrado então ele tem que está disponível no formato HTML", func() {
-			_, st, err := util.Get(boleto.URL, "", nil)
+			html, st, err := util.Get(boleto.URL, "", nil)
 			So(err, ShouldEqual, nil)
 			So(st, ShouldEqual, 200)
-			//htmlFromBoleto := strings.Contains(html, `<td class="boletoNumber center"><span>`+boleto.DigitableLine+`</span></td>`)
-			//So(htmlFromBoleto, ShouldBeTrue)
+			htmlFromBoleto := strings.Contains(html, boleto.DigitableLine)
+			So(htmlFromBoleto, ShouldBeTrue)
 		})
 	})
 
