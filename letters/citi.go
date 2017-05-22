e96c25e56f127036da8d4de81e78a31b769afea5
Philippe Moneda <philippe.moneda@mundipagg.com>
2017-05-22 18:42:02 -0300

    :construction: ajusta a carta de integração com o citi de acordo com a documentação

--- letters/citi.go
+++ letters/citi.go
@@ -1,96 +1,83 @@
 package letters
 
 const responseCiti = `
-<?xml version="1.0" encoding="utf-8"?>
-<soapenv:Envelope 
-    xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/">
-    <soapenv:Body>
-        <manutencaocobrancabancaria:SERVICO_SAIDA 
-            xmlns:manutencaocobrancabancaria="http://caixa.gov.br/sibar/manutencao_cobranca_bancaria/boleto/externo" 
-            xmlns:sibar_base="http://caixa.gov.br/sibar">
-            <sibar_base:HEADER>
-                <VERSAO>1.0</VERSAO>
-                <OPERACAO>{{operation}}</OPERACAO>
-                <DATA_HORA>{{datetime}}</DATA_HORA>
-            </sibar_base:HEADER>
-            <COD_RETORNO>{{returnCode}}</COD_RETORNO>
-            <MSG_RETORNO>{{returnMessage}}</MSG_RETORNO>
-            <DADOS>
-                <EXCECAO>{{exception}}</EXCECAO>
-                <CODIGO_BARRAS>{{barcodeNumber}}</CODIGO_BARRAS>
-                <LINHA_DIGITAVEL>{{digitableLine}}</LINHA_DIGITAVEL>
-                <NOSSO_NUMERO>{{ourNumber}}</NOSSO_NUMERO>
-                <URL>{{url}}</URL>
-            </DADOS>
-        </manutencaocobrancabancaria:SERVICO_SAIDA>
-    </soapenv:Body>
-</soapenv:Envelope>
+<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/">
+   <soapenv:Header/>
+   <soapenv:Body>
+      <RegisterBoletoResponse>
+         <actionCode>{{returnCode}}</actionCode>
+         <reasonMessage>{{returnMessage}}</reasonMessage>
+      </RegisterBoletoResponse>
+   </soapenv:Body>
+</soapenv:Envelope>>
 `
 
 const registerBoletoCiti = `
 
-## SOAPAction:IncluiBoleto
+## SOAPAction:RegisterBoleto
 ## Content-Type:text/xml
 
-<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:ext="http://caixa.gov.br/sibar/manutencao_cobranca_bancaria/boleto/externo" xmlns:sib="http://caixa.gov.br/sibar">
+<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/">
    <soapenv:Header/>
    <soapenv:Body>
-      <ext:SERVICO_ENTRADA>
-         <sib:HEADER>
-            <VERSAO>1.0</VERSAO>
-            <!--Optional:-->
-            <AUTENTICACAO>{{.Authentication.AuthorizationToken}}</AUTENTICACAO>
-            <USUARIO>1234567</USUARIO>
-            <OPERACAO>INCLUI_BOLETO</OPERACAO>
-            <SISTEMA_ORIGEM>SIGCB</SISTEMA_ORIGEM>
-            <DATA_HORA>{{fullDate today}}</DATA_HORA>
-         </sib:HEADER>
-         <DADOS>
-            <!--You have a CHOICE of the next 3 items at this level-->
-            <INCLUI_BOLETO>
-               <CODIGO_BENEFICIARIO>{{.Agreement.AgreementNumber}}</CODIGO_BENEFICIARIO>
-               <TITULO>                  
-                  <NOSSO_NUMERO>{{.Title.OurNumber}}</NOSSO_NUMERO>
-                  <NUMERO_DOCUMENTO>{{.Title.DocumentNumber}}</NUMERO_DOCUMENTO>
-                  <DATA_VENCIMENTO>{{enDate .Title.ExpireDateTime "-"}}</DATA_VENCIMENTO>
-                  <VALOR>{{.Title.AmountInCents}}</VALOR>
-                  <TIPO_ESPECIE>17</TIPO_ESPECIE>
-                  <FLAG_ACEITE>S</FLAG_ACEITE>   
-                  <JUROS_MORA>
-                     <TIPO>ISENTO</TIPO>
-                     <VALOR>0</VALOR>                                                        
-                  </JUROS_MORA>
-                  <POS_VENCIMENTO>
-                     <ACAO>DEVOLVER</ACAO>
-                     <NUMERO_DIAS>30</NUMERO_DIAS>
-                  </POS_VENCIMENTO>                       
-                  <CODIGO_MOEDA>9</CODIGO_MOEDA>
-                  <PAGADOR>
-                     <!--You have a CHOICE of the next 2 items at this level-->
-                     {{if eq .Buyer.Document.Type "CPF"}}
-					 	<CPF>{{.Buyer.Document.Number}}</CPF>
-                     	<NOME>{{.Buyer.Name}}</NOME>
-                     {{else}}
-					 	<CNPJ>{{.Buyer.Document.Number}}</CNPJ>
-                     	<RAZAO_SOCIAL>{{.Buyer.Name}}</RAZAO_SOCIAL>
-					 {{end}}
-                     <ENDERECO>
-                        <LOGRADOURO>{{.Buyer.Address.Street}} {{.Buyer.Address.Number}} {{.Buyer.Address.Complement}}</LOGRADOURO>
-                        <BAIRRO>{{.Buyer.Address.District}}</BAIRRO>
-                        <CIDADE>{{.Buyer.Address.City}}</CIDADE>
-                        <UF>{{.Buyer.Address.StateCode}}</UF>
-                        <CEP>{{.Buyer.Address.ZipCode}}</CEP>
-                     </ENDERECO>
-                  </PAGADOR>                                                                       
-                  <FICHA_COMPENSACAO>
-                     <MENSAGENS>
-                        <MENSAGEM>{{.Title.Instructions}}</MENSAGEM>
-                     </MENSAGENS>
-                  </FICHA_COMPENSACAO>                  
-               </TITULO>
-            </INCLUI_BOLETO>
-         </DADOS>
-      </ext:SERVICO_ENTRADA>
+      <GrpREMColTit>
+         <GrpBenf>
+            <CdClrSys>745</CdClrSys>
+            <CdIspb>33479023</CdIspb>
+            <CdtrId>{{.Agreement.AgreementNumber}}</CdtrId>
+            <CdtrNm>{{.Recipient.Name}}</CdtrNm>
+            <CdtrTaxId>{{.Recipient.Document.Number}}</CdtrTaxId>
+            <CdtrTaxTp>J</CdtrTaxTp>
+         </GrpBenf>
+         <GrpClPgd>
+            <DbtrNm>{{.Buyer.Name}}</DbtrNm>
+            {{if .Buyer.Document.Type eq "CPF"}}				
+            	<DbtrTaxTp>F</DbtrTaxTp>
+			{{else}}
+            	<DbtrTaxTp>J</DbtrTaxTp>
+			{{end}}
+			<DbtrTaxId>{{.Buyer.Document.Number}}</DbtrTaxId>
+            <GrpClPgdAdr>
+               <DbtrAdrTp>{{.Buyer.Address.Street}} {{.Buyer.Address.Number}} {{.Buyer.Address.Complement}}</DbtrAdrTp>
+               <DbtrCtrySubDvsn>{{.Buyer.Address.StateCode}}</DbtrCtrySubDvsn>
+               <DbtrPstCd>{{.Buyer.Address.ZipCode}}</DbtrPstCd>
+               <DbtrTwnNm>{{.Buyer.Address.City}}</DbtrTwnNm>
+            </GrpClPgdAdr>
+         </GrpClPgd>
+         <CdOccTp>01</CdOccTp>
+         <DbtrGrntNm />
+         <DbtrMsg>{{.Title.Instructions}}</DbtrMsg>
+         <TitlAmt>{{.Title.AmountInCents}}</TitlAmt>
+         <TitlBarCdInd>0</TitlBarCdInd>
+         <TitlCcyCd>09</TitlCcyCd>
+         <TitlCiaCdId>{{.Title.DocumentNumber}}</TitlCiaCdId>
+         <TitlDueDt>{{enDate .Title.ExpireDate "-"}}</TitlDueDt>
+         <TitlInstrNmDtExec>0</TitlInstrNmDtExec>
+         <TitlInstrProtInd> </TitlInstrProtInd>
+         <TitlInstrWrtOffInd> </TitlInstrWrtOffInd>
+         <TitlIOFAmt>0</TitlIOFAmt>
+         <TitlIssDt>{{enDate today "-"}}</TitlIssDt>
+         <TitlOurNb>{{padLeft (toString .Title.OurNumber) "0" 12}}</TitlOurNb>
+         <TitlPortCd>1</TitlPortCd>
+         <TitlRbtAmt>0</TitlRbtAmt>
+         <TitlTpCd>03</TitlTpCd>
+         <TitlYourNb>{{.Title.DocumentNumber}}</TitlYourNb>
+         <GrpDscnt>
+            <TitlDscntAmtOrPrct>0</TitlDscntAmtOrPrct>
+            <TitlDscntEndDt> </TitlDscntEndDt>
+            <TitlDscntTp> </TitlDscntTp>
+         </GrpDscnt>
+         <GrpItrs>
+            <TitlItrsAmtOrPrct>0</TitlItrsAmtOrPrct>
+            <TitlItrsStrDt> </TitlItrsStrDt>
+            <TitlItrsTp> </TitlItrsTp>
+         </GrpItrs>
+         <GrpFn>
+            <TitlFnAmtOrPrct>0</TitlFnAmtOrPrct>
+            <TitlFnStrDt> </TitlFnStrDt>
+            <TitlFnTp> </TitlFnTp>
+         </GrpFn>
+      </GrpREMColTit>
    </soapenv:Body>
 </soapenv:Envelope>
 `
