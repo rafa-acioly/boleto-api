7270990b52d2e6340782d9a6a44f4ed1d5e6c45f
Paulo Lima <paulolimarb@gmail.com>
2017-08-08 18:09:35 -0300

    Corrige lógica de validação.

--- citibank/validations.go
+++ citibank/validations.go
@@ -36,7 +36,7 @@ func citiValidateAccount(b interface{}) error {
 func citiValidateAccountDigit(b interface{}) error {
 	switch t := b.(type) {
 	case *models.BoletoRequest:
-		if len(t.Agreement.AccountDigit) < 1 && len(t.Agreement.AccountDigit) > 2{
+		if len(t.Agreement.AccountDigit) < 1 || len(t.Agreement.AccountDigit) > 2{
 			return errors.New(fmt.Sprintf("O digito da conta precisa ser preenchido."))
 		}
 		return nil
